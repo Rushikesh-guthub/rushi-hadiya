cf4b04b67f67e0eee4e8436b7078d5f650050240
Himanshu-0711 <himanshuwagh23@gmail.com>
2024-07-15 11:17:54 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent implements OnInit {
 title = 'hadiya_products_admin';
 constructor(private http: HttpClient) {}
 ngOnInit() {
-this.http.get('http://hadiya-backend-alb-1512806879.us-east-1.elb.amazonaws.com:3000').subscribe((data: any) => {
+this.http.get('http://hadiya-backend-alb-1512806879.us-east-1.elb.amazonaws.com').subscribe((data: any) => {
 console.log(data);
 });
 }
