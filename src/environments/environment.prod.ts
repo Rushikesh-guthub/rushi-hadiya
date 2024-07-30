e3f2326272135010f9b1e863f1ee8aaf53994991
Himanshu-0711 <himanshuwagh23@gmail.com>
2024-07-30 12:13:40 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiGatewayURL: 'http://backend-lb-2142802251.us-east-1.elb.amazonaws.com/'
+  apiGatewayURL: 'http://backend-lb-2142802251.us-east-1.elb.amazonaws.com'
 };
