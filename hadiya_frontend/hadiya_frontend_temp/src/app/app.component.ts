b3c9c03ef7f31676c20bc6f3edc6f1f89e039836
root <root@hotstar.hixbpjvjibmezh0dv5ont55hae.jx.internal.cloudapp.net>
2024-08-02 06:54:08 +0000

    Add files to hadiya_frontend

---
+++ hadiya_frontend/hadiya_frontend_temp/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+title = 'hadiya_products_admin';
+constructor(private http: HttpClient) {}
+ngOnInit() {
+this.http.get('http://backend-lb-2142802251.us-east-1.elb.amazonaws.com').subscribe((data: any) => {
+console.log(data);
+});
+}
+}
